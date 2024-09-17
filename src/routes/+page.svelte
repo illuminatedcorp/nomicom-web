81822cd2aa48fb76b18416eed44694918ac3debd
Fryke <tmktahu@gmail.com>
2024-09-16 23:19:16 -0700

    styled front page a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { base } from '$app/paths';
 
 	import { useApi } from '$lib/models/useApi.js';
+	import Button from '@/components/ui/button/button.svelte';
 	const { apiCall } = useApi();
 
 	onMount(async () => {
@@ -11,33 +13,44 @@
 	});
 </script>
 
-<div class="flex justify-center">
+<div class="flex flex-col items-center">
 	<div
-		class="flex justify-center bg-background-900 bg-opacity-70 border-2 border-background-900 rounded-md px-10 py-5 mx-24 mt-10"
+		class="flex flex-col justify-center bg-background-900 bg-opacity-80 border-2 border-background-900 rounded-md px-10 py-5 mt-10"
 	>
 		<div class="flex flex-col items-center">
-			<div class="text-xl text-left w-full">We are recruiting!</div>
-			<div class="w-full text-left">
+			<div class="text-center">
 				Illuminated is a PVP focused corporation in Eve Online and a member of Goonswarm Federation
-				based in Delve. We are looking for active PVP pilots and industrialists to join our ranks.
-				If you are interested in joining, please join our Discord server and ask for a recruiter.
+				based in Delve.
+				<br />
+				We are looking for active PVP pilots and industrialists to join our ranks.
 			</div>
-
-			<div class="text-xl text-left w-full mt-5">We will likely be a good fit if:</div>
-			<ul class="w-full text-left">
-				<li>You enjoy PVP</li>
-				<li>You are an active player</li>
-				<li>You've heard about Goonswarm's SIGs and want in</li>
-				<li>Want to be apart of one of the greatest stories in Eve</li>
-			</ul>
-
-			<div class="text-xl text-left w-full mt-5">What we have to offer:</div>
-			<ul class="w-full text-left">
-				<li>Corp PVP fleets</li>
-				<li>Access to massive industrial infrastructure</li>
-				<li>Active comms</li>
-				<li>Experienced leadership</li>
-			</ul>
 		</div>
+	</div>
+
+	<div
+		class="flex flex-col justify-center bg-background-900 bg-opacity-80 border-2 border-background-900 rounded-md px-10 py-5 mt-5 w-2/5 min-w-fit"
+	>
+		<div class="text-2xl text-center">You will do well with us if:</div>
+		<div class="mt-3">You enjoy PVP</div>
+		<div>You are an active player</div>
+		<div>You've heard about Goonswarm's SIGs and want in</div>
+		<div>Want to be apart of one of the greatest stories in Eve</div>
+	</div>
+
+	<div
+		class="flex flex-col justify-center bg-background-900 bg-opacity-80 border-2 border-background-900 rounded-md px-10 py-5 mt-5 w-1/4 min-w-fit"
+	>
+		<div class="text-2xl text-center">What we have to offer:</div>
+		<div class="mt-3">Corp PVP fleets</div>
+		<div>Access to massive industrial infrastructure</div>
+		<div>Active comms & community</div>
+		<div>Experienced leadership</div>
+	</div>
+
+	<div
+		class="flex flex-col justify-center bg-background-900 bg-opacity-80 border-2 border-background-900 rounded-md px-10 py-5 mt-5 min-w-fit"
+	>
+		<div class="text-2xl text-center">Interested?</div>
+		<Button href="{base}/join" target="_blank" class="text-xl py-5 px-8 mt-4">Join Today</Button>
 	</div>
 </div>
