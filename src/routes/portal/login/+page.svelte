990c9a18c27d4e4bd8e64ed133bc43a268918c6a
Fryke <tmktahu@gmail.com>
2024-09-17 11:55:30 -0700

    fixing missing base in login redirect

--- src/routes/portal/login/+page.svelte
+++ src/routes/portal/login/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { v4 as uuidv4 } from 'uuid';
+	import { base } from '$app/paths';
 
-	let redirectUri = encodeURIComponent(import.meta.env.VITE_APP_HOST + '/portal/login/redirect');
+	let redirectUri = encodeURIComponent(
+		import.meta.env.VITE_APP_HOST + base + '/portal/login/redirect'
+	);
 	let clientId = import.meta.env.VITE_EVE_APP_CLIENT_ID;
 	let uuidState = uuidv4();
 	let loginUrl = `https://login.eveonline.com/v2/oauth/authorize/?response_type=code&redirect_uri=${redirectUri}&client_id=${clientId}&state=${uuidState}`;
