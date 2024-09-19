fb3ad7c788b838f075fd25baaa3acc2e2f0935aa
Fryke <tmktahu@gmail.com>
2024-09-19 12:18:07 -0700

    lets see if this works

--- src/lib/models/useApi.ts
+++ src/lib/models/useApi.ts
@@ -1,22 +1,33 @@
 import { goto } from '$app/navigation';
 import { base } from '$app/paths';
 import axios from 'axios';
-import { WEB_ROUTES } from './useConstants';
+import { API_ROUTES, WEB_ROUTES } from './useConstants';
 
 export const useApi = () => {
 	// so the idea here is that we simple forward all API requests, their route labels, and params to the sveltkit server
 	const apiCall = async (apiRoute, params) => {
 		try {
-			const response = await axios.post(
-				import.meta.env.VITE_SERVER_HOST + '/api/v1' + apiRoute,
-				params,
-				{
+			let response;
+			if (apiRoute === API_ROUTES.login) {
+				response = await axios.post(import.meta.env.VITE_SERVER_HOST + '/api/v1' + apiRoute, null, {
+					params,
 					headers: {
 						'Content-Type': 'application/json'
 					},
 					withCredentials: true // Include cookies with the request
-				}
-			);
+				});
+			} else {
+				response = await axios.post(
+					import.meta.env.VITE_SERVER_HOST + '/api/v1' + apiRoute,
+					params,
+					{
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						withCredentials: true // Include cookies with the request
+					}
+				);
+			}
 
 			const data = response.data;
 
