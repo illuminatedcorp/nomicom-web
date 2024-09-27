42fa83acfdbc78663364e3dd6bfc793473af0fd8
Fryke <tmktahu@gmail.com>
2024-09-26 17:40:49 -0700

    nevermind hold on

--- src/lib/models/useRoles.ts
+++ src/lib/models/useRoles.ts
@@ -12,7 +12,7 @@ export const useRoles = () => {
 		let newRole = {
 			name: 'New Role',
 			description: 'New Role Description',
-			permissions: ['testing']
+			permissions: []
 		};
 
 		let response = await apiCall(API_ROUTES.createRole, newRole);
