7a2f11f489bc08cfbd015c0d1fb3a82afd8ae2d4
Fryke <tmktahu@gmail.com>
2024-10-19 12:06:38 -0700

    disabling build thing for now

--- src/routes/nomicon/wiki/[slug]/+page.server.ts
+++ src/routes/nomicon/wiki/[slug]/+page.server.ts
@@ -3,8 +3,9 @@ const { getWikiIndex } = useWiki();
 
 /** @type {import('./$types').EntryGenerator} */
 export async function entries() {
-	const index = await getWikiIndex();
-	const slugs = index.map((entry) => ({ slug: entry.slug }));
+	// const index = await getWikiIndex();
+	// const slugs = index.map((entry) => ({ slug: entry.slug }));
+	const slugs = [{ slug: 'temp' }];
 
 	return slugs;
 }
